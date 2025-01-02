7a87afd208a175927be8c589725c5dc6df45b741
Matthew Maennche <matthew@maennche.com>
2025-01-02 16:50:07 -0600

    Update faq.vue

--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -1,1 +1,41 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('faq', () => queryContent('/faq').findOne())
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
 
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
+
+defineOgImageComponent('Saas')
+
+</script>
+
+<template>
+  <div v-if="page">
+    <UPageHero v-bind="page.hero">
+      <template #links>
+        <UPricingToggle
+          v-model="isYearly"
+          class="w-48"
+        />
+      </template>
+    </UPageHero>
+
+    <ULandingSection
+      :title="page.faq.title"
+      :description="page.faq.description"
+    >
+      <ULandingFAQ
+        :items="page.faq.items"
+        multiple
+        default-open
+        class="max-w-4xl mx-auto"
+      />
+    </ULandingSection>
+  </div>
+</template>
